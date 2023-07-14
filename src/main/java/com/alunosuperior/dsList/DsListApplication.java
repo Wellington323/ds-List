efc3e78813667fb7ba9be50b69d94f632e071941
Wellington Silva <s37v.1@hotmail.com>
2023-07-13 21:54:20 -0300

    Project Created

---
+++ src/main/java/com/alunosuperior/dsList/DsListApplication.java
@@ -0,0 +1,13 @@
+package com.alunosuperior.dsList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alunosuperior/dsList/DsListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alunosuperior.dsList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
